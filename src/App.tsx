fbe79e9e4c330acb8f91ce7bc5c4ffa9cddbab9a
HOAN <hoan.c9907@gmail.com>
2026-07-14 23:34:18 +0900

    feat: tailwind 설치 및 기본 레이아웃 구성

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,21 @@
 function App() {
-  return <main className="app">App</main>
+  return (
+    <div
+      className="
+    mx-auto
+    grid
+    min-h-dvh
+    w-full
+    max-w-[480px]
+    grid-rows-[minmax(0,1fr)]
+    bg-white
+  "
+    >
+      <main className="min-w-0">
+        <div className="w-full p-5">App</div>
+      </main>
+    </div>
+  );
 }
 
-export default App
+export default App;
